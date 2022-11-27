38b8494c233ee0934657801aae461fb829fedf39
olaoluwa402 <ibukundaniel402@yahoo.co.uk>
2022-11-27 20:48:43 +0100

    fixed home slider bug

--- src/components/Home/Hero/Hero.js
+++ src/components/Home/Hero/Hero.js
@@ -14,41 +14,43 @@ function Hero() {
 
   return (
     <div className={styles.container}>
-      <Carousel
-        showArrows={true}
-        autoPlay={true}
-        thumbWidth="0px"
-        showThumbs={false}
-        infiniteLoop={true}
-        interval="5000"
-        showStatus={false}
-      >
-        {sliders &&
-          sliders.map((slide, i) => (
-            <div key={i}>
-              <div
-                className={styles.container}
-                style={{
-                  backgroundImage: `url(${slide.image})`,
-                  backgroundPosition: "center",
-                  backgroundSize: "cover",
-                  width: "100%",
-                }}
-              >
-                <div className={styles.content}>
-                  <h1 className={styles.hero_heading}>{slide.title}</h1>
-                  <p className={styles.hero_message}>{slide.description}</p>
-                  <Link href={slide.url}>
-                    <a className={styles.hero__cta}>
-                      {settings[0].getQuoteCTA}
-                    </a>
-                  </Link>
+      <div className="overflow-hidden">
+        <Carousel
+          showArrows={true}
+          autoPlay={true}
+          thumbWidth="0px"
+          showThumbs={false}
+          infiniteLoop={true}
+          interval="5000"
+          showStatus={false}
+        >
+          {sliders &&
+            sliders.map((slide, i) => (
+              <div key={i}>
+                <div
+                  className={styles.container}
+                  style={{
+                    backgroundImage: `url(${slide.image})`,
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                    width: "100%",
+                  }}
+                >
+                  <div className={styles.content}>
+                    <h1 className={styles.hero_heading}>{slide.title}</h1>
+                    <p className={styles.hero_message}>{slide.description}</p>
+                    <Link href={slide.url}>
+                      <a className={styles.hero__cta}>
+                        {settings[0].getQuoteCTA}
+                      </a>
+                    </Link>
+                  </div>
+                  <div className={styles.overlay}></div>
                 </div>
-                <div className={styles.overlay}></div>
               </div>
-            </div>
-          ))}
-      </Carousel>
+            ))}
+        </Carousel>
+      </div>
 
       <div className="hidden lg:block w-full mx-auto absolute bottom-12 left-0 right-0">
         <HeroSelecBar />
