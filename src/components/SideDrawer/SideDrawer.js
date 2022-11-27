381cbbc91dffa7d7aadde98ca4bcc0a46dcce24c
olaoluwa402 <ibukundaniel402@yahoo.co.uk>
2022-11-27 12:20:09 +0100

    hhj

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -1,71 +1,47 @@
 import React from "react";
 import { MdContactPhone } from "react-icons/md";
-import {
-  RiServiceFill,
-} from "react-icons/ri";
-import {GiPayMoney,GiFamilyHouse} from 'react-icons/gi'
-import {BsInfoSquareFill} from "react-icons/bs"; 
+import { RiServiceFill } from "react-icons/ri";
+import { GiPayMoney, GiFamilyHouse } from "react-icons/gi";
+import { BsInfoSquareFill } from "react-icons/bs";
+import { useSelector } from "react-redux";
 import logo from "../../asset/images/titanium-logo.png";
 import Image from "next/image";
 import Link from "next/link";
 
-const SideDrawer = ({show, setSideDrawerOpen}) => {
+const SideDrawer = ({ show, setSideDrawerOpen }) => {
+  const store = useSelector((store) => store.homeData.data);
+  const { menuItems } = store;
+
   let drawerClasses = "side-drawer";
   if (show) {
     drawerClasses = "side-drawer open";
-  } 
+  }
 
-  return ( 
+  return (
     <React.Fragment>
-      <nav className={drawerClasses}> 
+      <nav className={drawerClasses}>
         <div className="drawerClasses-logo__wrapper">
           <h1 className="side-drawer-logo">
             <Image src={logo} alt="Titanium" width={200} height={120} />
           </h1>
         </div>
         <ul className="dropdown-user-icon" id="">
-          <li className="dropdown">
-            <div className="dropdownContainer">
-              <BsInfoSquareFill className="dropdownIcon" />
-              <Link href="/">
-                <a className="dropdown_Link" onClick={()=> setSideDrawerOpen((prev)=> !prev)}>About us</a> 
-              </Link>
-            </div>
-          </li>
-          <li className="dropdown">
-            <div className="dropdownContainer">
-              <RiServiceFill className="dropdownIcon" />
-              <Link href="/">
-                <a className="dropdown_Link" onClick={()=> setSideDrawerOpen((prev)=> !prev)}>Services</a>
-              </Link>
-            </div> 
-          </li>
-          <li className="dropdown">
-            <div className="dropdownContainer">
-              <GiPayMoney className="dropdownIcon" />
-              <Link href="/">
-                <a className="dropdown_Link" onClick={()=> setSideDrawerOpen((prev)=> !prev)}>Investment Club</a>
-              </Link>
-            </div> 
-          </li>
-
-          <li className="dropdown">
-            <div className="dropdownContainer">
-              <GiFamilyHouse className="dropdownIcon" />
-              <Link href="/">
-                <a className="dropdown_Link" onClick={()=> setSideDrawerOpen((prev)=> !prev)}>Properties</a>
-              </Link>
-            </div>
-          </li>
-
-          <li className="dropdown">
-            <div className="dropdownContainer">
-              <MdContactPhone className="dropdownIcon" />
-              <Link href="/">
-                <a className="dropdown_Link" onClick={()=> setSideDrawerOpen((prev)=> !prev)}>Contact Us</a>
-              </Link>
-            </div>
-          </li>
+          {menuItems &&
+            menuItems.map((item) => (
+              <li key={item.id} className="dropdown">
+                <div className="dropdownContainer">
+                  <BsInfoSquareFill className="dropdownIcon" />
+                  <Link href={item.url}>
+                    <a
+                      className="dropdown_Link"
+                      onClick={() => setSideDrawerOpen((prev) => !prev)}
+                    >
+                      {item.name}
+                    </a>
+                  </Link>
+                </div>
+              </li>
+            ))}
         </ul>
       </nav>
     </React.Fragment>
